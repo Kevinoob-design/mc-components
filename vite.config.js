83a809e360acd38669916776abc431b5eab73ab3
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2024-03-22 11:44:43 -0400

    fix: export default

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
 				// Provide global variables to use in the UMD build
 				// for externalized deps
 				globals: {
-					vue: 'angular',
+					angular: 'angular',
 				},
 			},
 			plugins: [
