ba6cd1e2bd43624beac70b42fd8f8a78bf56534e
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2024-03-22 11:41:09 -0400

    fix: export default

--- app/components/components.module.ts
+++ app/components/components.module.ts
@@ -1,12 +1,10 @@
 import angular from 'angular';
 import { PrimaryButtonModule } from './buttons/index';
 
-const ComponentsModule = angular.module('McComponents', [
+export const ComponentsModule = angular.module('McComponents', [
 	'ngAnimate',
 	'ngAria',
 	'ngMessages',
 	'ngMaterial',
 	PrimaryButtonModule,
 ]).name;
-
-export default ComponentsModule
