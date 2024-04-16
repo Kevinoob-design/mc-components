f5a2b852f7ce5f5d1c4556fc6f1b3c49149c5e06
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2024-04-16 12:26:13 -0400

    some

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,6 +1,12 @@
 import './app.html';
+class controller {
+	click() {
+		alert('Hello, World!')
+	}
+}
 
 export const WebAppComponent = {
 	bindings: {},
 	templateUrl: 'app/app.html',
+	controller: controller
 };
