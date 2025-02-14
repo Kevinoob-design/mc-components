9a694d1912ed1ed8266777e30f5e1cdc3ece086c
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2025-02-14 03:26:52 -0400

    refactor(icons): refactor selector inner locator
    done:
    
    - added e2e for the missing icons, and refactor inner locator to be taken when accessing the iframe
    
    task: n/a

---
+++ e2e/icons/icon-sort-down/icon-sort-down.spec.ts
@@ -0,0 +1,43 @@
+import { test, expect } from '@playwright/test'
+import {
+	sbLocatorGetButton,
+	sbLocatorGetByRoleName,
+	sbLocatorGetComboBox,
+	sbLocatorGetHeading,
+	sbRoleType
+} from '../../storybook.locator'
+import { STORYBOOK_DOCS_PATH, STORYBOOK_URL } from '../../storybook.constants'
+
+test.describe('Icon-Sort-Down Story', () => {
+	const sbInnerLocator = '#story--library-icons-icon-sort-down--default--primary-inner'
+
+	test.beforeEach(async ({ page }) => {
+		await page.goto(`${STORYBOOK_URL}/${STORYBOOK_DOCS_PATH}/library-icons-icon-sort-down--docs`)
+	})
+
+	test('Should have name Icon-Sort-Down', async ({ page }) => {
+		await expect(sbLocatorGetHeading(page, 'Icon-Sort-Down')).toHaveText('Icon-Sort-Down')
+	})
+
+	test('Should have svg icon', async ({ page }) => {
+		await expect(sbLocatorGetButton(page).first().getByRole(sbRoleType.IMAGE)).toBeVisible()
+	})
+
+	test('Should change svg size', async ({ page }) => {
+		const className = 'w-10'
+
+		await sbLocatorGetComboBox(page).nth(0).selectOption(className)
+		await expect(sbLocatorGetByRoleName(page, sbRoleType.IMAGE, '', sbInnerLocator)).toHaveClass(
+			new RegExp(className)
+		)
+	})
+
+	test('Should change icon color', async ({ page }) => {
+		const className = 'fill-red-900'
+
+		await sbLocatorGetComboBox(page).nth(1).selectOption(className)
+		await expect(sbLocatorGetByRoleName(page, sbRoleType.IMAGE, '', sbInnerLocator)).toHaveClass(
+			new RegExp(className)
+		)
+	})
+})
