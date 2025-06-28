293f472078791ce88c14b27afc37e3fb833c973b
Hector Morales <52089890+Kevinoob-design@users.noreply.github.com>
2025-06-28 17:14:50 -0400

    fix(infra): terraform paths
    done:
    
    - fix terraform paths on some layers when requiring modules
    
    task: n/a

--- terraform/layers/L20_registries/main.tf
+++ terraform/layers/L20_registries/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "npm_registry" {
-  source          = "../../../modules/codeartifact"
+  source          = "../../modules/codeartifact"
   domain_name     = var.domain_name
   repository_name = var.repository_name
   description     = var.description
--- terraform/layers/L30_storage/main.tf
+++ terraform/layers/L30_storage/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "storybook_bucket" {
-  source      = "../../../modules/s3/s3_bucket"
+  source      = "../../modules/s3/s3_bucket"
   bucket_name = "mc-storybook-bucket"
   tags = {
     usage = "Storybook static files"
@@ -11,12 +11,12 @@ module "storybook_bucket" {
 }
 
 module "storybook_public_access_block" {
-  source      = "../../../modules/s3/s3_public_access_block"
+  source      = "../../modules/s3/s3_public_access_block"
   bucket_name = module.storybook_bucket.aws_s3_bucket_name
 }
 
 module "storybook_website_config" {
-  source = "../../../modules/s3/s3_website_config"
+  source = "../../modules/s3/s3_website_config"
 
   bucket_name           = module.storybook_bucket.aws_s3_bucket_name
   index_document_suffix = "index.html"
